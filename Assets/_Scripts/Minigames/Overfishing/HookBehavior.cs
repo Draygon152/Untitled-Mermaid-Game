fbcf08953402ad29922bfc23f60a149b6c506265
Dashiell Curran <dcurran@usc.edu>
2024-02-12 18:46:04 -0800

    Overfishing minigame

---
+++ Assets/_Scripts/Minigames/Overfishing/HookBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookBehavior : MonoBehaviour
+{
+    public float descentSpeed = 5f;
+    public bool caughtFish = false;
+
+    void Update()
+    {
+        if (caughtFish)
+        {
+            transform.Translate(Vector2.up * descentSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.down * descentSpeed * Time.deltaTime);
+        }
+        if (transform.position.y > 13)
+        {
+            RemoveHook();
+        }
+    }
+
+    public void RemoveHook()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bound"))
+        {
+            RemoveHook();
+        }
+    }
+}
